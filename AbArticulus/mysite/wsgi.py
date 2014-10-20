032c4f97de9abed418c807bc6875d3792ab9021e
JBKahn <jkahn@waveaccounting.com>
2014-10-19 20:12:20 -0600

    environment variable to make dev easier

--- AbArticulus/mysite/wsgi.py
+++ AbArticulus/mysite/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.getenv('DJANGO_SETTINGS_MODULE', "mysite.development"))
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
