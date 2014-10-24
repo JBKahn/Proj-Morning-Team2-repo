db6deb5be5d23e3ff59fb2bf26a0fdc23cc96770
LLay <lay.lyon@gmail.com>
2014-10-23 22:48:48 -0400

    forgot to customUser model in authentication

--- AbArticulus/authentication/models.py
+++ AbArticulus/authentication/models.py
@@ -1,9 +1,8 @@
 # Define a custom User class to work with django-social-auth
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from abcalendar.models import Tag
 
 class CustomUser(AbstractUser):
     uoft_email = models.EmailField(max_length=70, blank=True)
-    userID = models.CharField(max_length=255) #this line necessary?
-    tags = models.foreignKey(abcalendar.tag)
+    tags = models.ManyToManyField(Tag)
