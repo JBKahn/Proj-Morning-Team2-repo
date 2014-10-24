ba5086fc42e585c22624124c2eb9ff7ece899e65
LLay <lay.lyon@gmail.com>
2014-10-24 14:55:57 -0400

    grammar

--- AbArticulus/abcalendar/models.py
+++ AbArticulus/abcalendar/models.py
@@ -6,30 +6,30 @@ from authentication.models import CustomUser
 
 class Event(models.Model):
     gevent_id = models.CharField(max_length=255)#Google event id
-    tag = models.foreignKey(Tag)
-    user = models.foreignKey(CustomUser)
+    tag = models.ForeignKey(Tag)
+    user = models.ForeignKey(CustomUser)
 
 
 class Tag(models.Model):
-    number = models.integerField(default=1, min_value=0)
-    tagType = models.CharField(max_length=25, choices=TAG_CHOICES)
-    organization = models.foreignKey(Organization)
+    number = models.IntegerField(default=1, min_value=0)
+    tag_type = models.CharField(max_length=25, choices=TAG_CHOICES)
+    organization = models.ForeignKey(Organization)
 
 
 class Comment(models.Model):
-    event = models.foreignKey(Event)
-    user = models.foreignKey(CustomUser)
+    event = models.ForeignKey(Event)
+    user = models.ForeignKey(CustomUser)
     comment = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
     
     
 class Vote(models.Model):
-    user = models.foreignKey(CustomUser)
-    event = models.foreignKey(Event)
-    number = models.integerField(choices=VOTE_CHOICES)
+    user = models.ForeignKey(CustomUser)
+    event = models.ForeignKey(Event)
+    number = models.IntegerField(choices=VOTE_CHOICES)
 
 
 class Organization(models.Model):
     name = models.CharField(max_length=255)
-    user = models.foreignKey(CustomUser)
+    user = models.ForeignKey(CustomUser)
     
