bc932d5261669321f9187f21d45a473a48a9fb4c
LLay <lay.lyon@gmail.com>
2014-10-23 23:03:23 -0400

    minor fixes

--- AbArticulus/abcalendar/models.py
+++ AbArticulus/abcalendar/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-from constants.py import TAG_CHOICES 
+from constants.py import TAG_CHOICES
+from constants.py import VOTE_CHOICES ##from constants.py import * ??
 
 class Event(models.Model):
     gevent_id = models.CharField(max_length=255)#Google event id
     eventDetail = foreignKey(EventDetail)
     tag = foreignKey(Tag)
+    organizer = foreignKey(Organization)
 
 class Tag(models.Model):
     number = intergerFeild(default=1, min_value=0)
@@ -21,6 +23,6 @@ class Vote(models.Model):
     event = foreignKey(Event)
     number = intergerFeild(choices=VOTE_CHOICES)
 
-class Orginization(models.Model):
+class Organization(models.Model):
     name = models.CharFeild(max_length=255)
     
