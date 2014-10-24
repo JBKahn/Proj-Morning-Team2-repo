1d178723d33ee57457f6212ef9c573134cfcd68b
LLay <lay.lyon@gmail.com>
2014-10-23 23:10:02 -0400

    some more minor fixes

--- AbArticulus/abcalendar/models.py
+++ AbArticulus/abcalendar/models.py
@@ -1,16 +1,20 @@
 from django.db import models
+
 from constants.py import TAG_CHOICES
-from constants.py import VOTE_CHOICES ##from constants.py import * ??
+from constants.py import VOTE_CHOICES ##or just: from constants.py import * ??
+
 
 class Event(models.Model):
     gevent_id = models.CharField(max_length=255)#Google event id
     eventDetail = foreignKey(EventDetail)
     tag = foreignKey(Tag)
-    organizer = foreignKey(Organization)
+
 
 class Tag(models.Model):
     number = intergerFeild(default=1, min_value=0)
     tagType = models.CharField(max_length=25, choices=TAG_CHOICES, default='Homework')
+    organizer = foreignKey(Organization)
+
 
 class Comment(models.Model):
     event = foreignKey(Event)
@@ -18,10 +22,12 @@ class Comment(models.Model):
     comment = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
     
+    
 class Vote(models.Model):
     user = foreginKey(User)
     event = foreignKey(Event)
     number = intergerFeild(choices=VOTE_CHOICES)
+
 
 class Organization(models.Model):
     name = models.CharFeild(max_length=255)
--- AbArticulus/authentication/models.py
+++ AbArticulus/authentication/models.py
@@ -1,7 +1,9 @@
 # Define a custom User class to work with django-social-auth
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
 from abcalendar.models import Tag
+
 
 class CustomUser(AbstractUser):
     uoft_email = models.EmailField(max_length=70, blank=True)
