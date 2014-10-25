61349306407a4ab7cd865909c057fa8d5ffbdf5e
LLay <lay.lyon@gmail.com>
2014-10-24 23:56:56 -0400

    fixing errors

--- AbArticulus/abcalendar/models.py
+++ AbArticulus/abcalendar/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 from constants import TAG_CHOICES, VOTE_CHOICES
-from authentication.models import CustomUser
 
 
 class Organization(models.Model):
     name = models.CharField(max_length=255)
-    user = models.ForeignKey('authentication.models.CustomUser')
+    user = models.ForeignKey('authentication.CustomUser')
 
 
 class Tag(models.Model):
     tag_type = models.CharField(max_length=25, choices=TAG_CHOICES)
     organization = models.ForeignKey(Organization)
     number = models.IntegerField(default=1)
+
     def save(self, *args, **kwargs):
         if self.number < -1:
             raise ValueError("number must be greater than 0")
@@ -20,23 +20,19 @@ class Tag(models.Model):
 
 
 class Event(models.Model):
-    gevent_id = models.CharField(max_length=255)#Google event id
+    gevent_id = models.CharField(max_length=255)  # Google event id
     tag = models.ForeignKey(Tag)
-    user = models.ForeignKey('authentication.models.CustomUser')
+    user = models.ForeignKey('authentication.CustomUser')
 
 
 class Comment(models.Model):
     event = models.ForeignKey(Event)
-    user = models.ForeignKey('authentication.models.CustomUser')
+    user = models.ForeignKey('authentication.CustomUser')
     comment = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
-    
-    
+
+
 class Vote(models.Model):
-    user = models.ForeignKey('authentication.models.CustomUser')
+    user = models.ForeignKey('authentication.CustomUser')
     event = models.ForeignKey(Event)
     number = models.IntegerField(choices=VOTE_CHOICES)
-
-
-
-    
