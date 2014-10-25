124cefb0df156006e8010630c9ed4ef5637c7474
LLay <lay.lyon@gmail.com>
2014-10-24 23:22:00 -0400

    fixing errors

--- AbArticulus/abcalendar/models.py
+++ AbArticulus/abcalendar/models.py
@@ -1,35 +1,42 @@
 from django.db import models
 
-from constants.py import TAG_CHOICES, VOTE_CHOICES
+from constants import TAG_CHOICES, VOTE_CHOICES
 from authentication.models import CustomUser
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey('authentication.models.CustomUser')
+
+
+class Tag(models.Model):
+    tag_type = models.CharField(max_length=25, choices=TAG_CHOICES)
+    organization = models.ForeignKey(Organization)
+    number = models.IntegerField(default=1)
+    def save(self, *args, **kwargs):
+        if self.number < -1:
+            raise ValueError("number must be greater than 0")
+        super(Tag, self).save(*args, **kwargs)
 
 
 class Event(models.Model):
     gevent_id = models.CharField(max_length=255)#Google event id
     tag = models.ForeignKey(Tag)
-    user = models.ForeignKey(CustomUser)
-
-
-class Tag(models.Model):
-    number = models.IntegerField(default=1, min_value=0)
-    tag_type = models.CharField(max_length=25, choices=TAG_CHOICES)
-    organization = models.ForeignKey(Organization)
+    user = models.ForeignKey('authentication.models.CustomUser')
 
 
 class Comment(models.Model):
     event = models.ForeignKey(Event)
-    user = models.ForeignKey(CustomUser)
+    user = models.ForeignKey('authentication.models.CustomUser')
     comment = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
     
     
 class Vote(models.Model):
-    user = models.ForeignKey(CustomUser)
+    user = models.ForeignKey('authentication.models.CustomUser')
     event = models.ForeignKey(Event)
     number = models.IntegerField(choices=VOTE_CHOICES)
 
 
-class Organization(models.Model):
-    name = models.CharField(max_length=255)
-    user = models.ForeignKey(CustomUser)
+
     
--- AbArticulus/authentication/models.py
+++ AbArticulus/authentication/models.py
@@ -2,9 +2,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from abcalendar.models import Tag
+#from abcalendar.models import Tag
 
 
 class CustomUser(AbstractUser):
     uoft_email = models.EmailField(max_length=70, blank=True)
-    tags = models.ManyToManyField(Tag)
+    #tags = models.ManyToManyField(Tag)
