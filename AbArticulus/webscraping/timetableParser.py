20c56d1796751319689a81cdbae1130c63018efe
mdata2 <mitch.data@mail.utoronto.ca>
2014-10-26 02:23:55 -0400

    modified, fixed timetableparser py file.

--- AbArticulus/webscraping/timetableParser.py
+++ AbArticulus/webscraping/timetableParser.py
@@ -1,58 +1,80 @@
-import random
 import re
+import collections
+import random
 import requests
 from bs4 import BeautifulSoup
+from bs4 import NavigableString
 
-def cleanDescription(string):
-     stripString=string.split("\n")
-     stripSpace=stripString[0].rstrip(" ")
-     return re.sub(r'\xc2\xa0',"",stripSpace).rstrip(",")
- 
-def parseCourseTable(link):
-	''' Returns key, value pairs in the following format: {courseCode:[courseDesc, time, lec...] '''
-     dicts={}
+
+
+def cleanText(text):
+     ''' Returns a string of course description without the escape seq. characters (i.e \r,\n). '''
+     
+     regex = re.compile(r'[\n\r\t]')
+     stripString = regex.sub('', text).rstrip(",")
+     finalString = re.sub(r'\xc2\xa0',"",stripString)
+     return finalString
+
+
+def parseCourseTable(link): 
+     ''' Docstrings here '''
+
+     # index 0: course code
+     # index 2: course description
+     # index 3: lecture code
+     # index 5: times
+     # index 6: location
+     # index 7: instructor  
+     
+     listCode=[]    
+     courses = collections.defaultdict(list)     
      sep = requests.get(link)
      course_page = BeautifulSoup(sep.content)
      table = course_page.find('table')
-     for row in table.findAll('tr'):
-          cinfo = [", ".join(cell.findAll(text=True)) for cell in row.findAll('td')]
-          convert=[cleanDescription(inst.encode('utf-8')) for inst in cinfo]
+     finder=table.findAll('tr')
+     
+     for row in range(len(finder)-1):          
+          course_info = [", ".join(cell.findAll(text=True)) for cell in finder[row].findAll('td')]
+          otherSched = [", ".join(cell.findAll(text=True)) for cell in finder[row+1].findAll('td')]
           
-          if len(convert) > 9:
-               codeCode=cleanDescription(cinfo[0].encode('utf-8'))
-               courseDesc=cleanDescription(cinfo[2].encode('utf-8'))
-               lec=cleanDescription(cinfo[3].encode('utf-8'))
-               time=cleanDescription(cinfo[5].encode('utf-8'))
-               loc=cleanDescription(cinfo[6].encode('utf-8'))
-               instructor=cleanDescription(cinfo[7].encode('utf-8'))
-               if not dicts.has_key(codeCode):
-                    dicts[codeCode]=[courseDesc,lec,time,loc,instructor]
-               else:
-                    dicts[codeCode].append([courseDesc,time,lec,loc,instructor])
-                    
-                    
-     return dicts
+          # Handles the first course schedule offered.
+          if (len(course_info) > 2 and len(course_info) > 7) and not(re.match(r'\xc2\xa0',course_info[0].encode('utf-8'))):
+           
+               course_code=cleanText(course_info[0].encode('utf-8'))
+               courses[course_code].append([cleanText(course_info[index].encode('utf-8')) for index in [2,3,5,6,7]])
+            
+          # Retrieves the courses with more than one schedule time
+          if re.match(r'\xc2\xa0',otherSched[0].encode('utf-8')):
+               if not(re.match(r'\xc2\xa0',course_info[0].encode('utf-8'))):
+                    listCode.append(course_info[0])
+                     
+          # Handles other schedule courses offered.        
+          if (len(course_info) > 2 and len(course_info) > 7) and re.match(r'\xc2\xa0',course_info[0].encode('utf-8')):
+               course_code=cleanText(listCode[-1].encode('utf-8'))
+               courses[course_code].append([cleanText(course_info[index].encode('utf-8')) for index in [2,3,5,6,7]])
+        
 
-def buildDict(lists):
-     pass
+     return courses
+            
 
-def parseCourse(listInfo):
-    pass
-    
- 
-if __name__ == "__main__":
+def parseTimetable():
      Timetable = "http://www.artsandscience.utoronto.ca/ofr/timetable/winter/sponsors.htm"
-     r = requests.get(Timetable)
-     soup = BeautifulSoup(r.content)
+     requestTT = requests.get(Timetable)
+     soup = BeautifulSoup(requestTT.content)
     
-     coursesInformation={}
      for li in soup.findAll('li'):
-          i = li.find('a', href=True)
-          names = i.contents[0]
-          fullLink = i.get('href')
+          department = li.find('a', href=True)
+          getContents = department.contents[0]
+          fullLink = department.get('href')
+          
           if len(fullLink) > 10:
                continue
+          
           fixedLink = "http://www.artsandscience.utoronto.ca/ofr/timetable/winter/" + fullLink
-          parseCourseTable(fixedLink)
+          print parseCourseTable(fixedLink)     
+     
+    
 
-          print parseCourseTable(fixedLink)
+if __name__ == "__main__":
+
+     parseTimetable()
