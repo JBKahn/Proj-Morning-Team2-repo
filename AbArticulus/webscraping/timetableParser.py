8eb98fb74af6a109cbe6188ff42c4262e14fc665
mdata2 <mitch.data@mail.utoronto.ca>
2014-10-28 02:00:33 -0400

    more modifications, refractoring from previous version.

--- AbArticulus/webscraping/timetableParser.py
+++ AbArticulus/webscraping/timetableParser.py
@@ -9,82 +9,86 @@ from bs4 import NavigableString
 regex_for_strip = re.compile(r'[\n\r\t]')
 regex_for_empt = re.compile(r'\xc2\xa0')
 
-def cleanText(text):
-     ''' Returns a string of course description without the escape seq. characters (i.e \r,\n). '''
-     
-     regex = re.compile(r'[\n\r\t]')
-     stripString = regex.sub('', text).rstrip(",")
-     finalString = re.sub(r'\xc2\xa0',"",stripString)
-     return finalString
-
-
-def parseCourseTable(link): 
+def parse_course_offerings(link): 
      ''' Process link that contains information about a list of courses offered by department.
          Returns the course info as dictionaries where key is the course code and values representing course schedule offered. '''
 
+     sched_row = 0
      popular_courses=[]    
      courses_offered = collections.defaultdict(list)     
-     sep = requests.get(link)
-     course_page = BeautifulSoup(sep.content)
-     table = course_page.find('table')
-     tableContent=table.findAll('tr')
-     html_table_size = len(tableContent) - 1 
+     uoft_timetable_site = requests.get(link)
+     course_page = BeautifulSoup(uoft_timetable_site.content)
+     course_table = course_page.find('table')
+     table_content=course_table.findAll('tr')
+     html_table_size = len(table_content) - 1 
      
-     
-     for sched_row in range(html_table_size):
-          course_info = [", ".join(cell.findAll(text=True)) for cell in tableContent[sched_row].findAll('td')]
-          other_sched = [", ".join(cell.findAll(text=True)) for cell in tableContent[sched_row+1].findAll('td')]
-          main_sched_offered = get_main_sched_time(course_info, courses_offered, sched_row)
-          other_schedules = get_other_sched_offered(course_info, other_sched, popular_courses, courses_offered, sched_row)
+     while sched_row < html_table_size:
+          course_info = [", ".join(cell.findAll(text=True)) for cell in table_content[sched_row].findAll('td')]
+          other_sched = [", ".join(cell.findAll(text=True)) for cell in table_content[sched_row+1].findAll('td')]
+          cancelled = len(course_info) >= 5 and clean_text(course_info[4]) == "Cancel"
+          
+          if not cancelled:
+               main_sched_offered = get_main_sched_time(course_info, courses_offered, sched_row)        
+          other_schedules = merge_course_info(course_info, other_sched, popular_courses, courses_offered, sched_row) 
+          sched_row+=1
                
      return courses_offered
+
+def parse_timetable():
+     ''' Returns a collection of courses offered by department. '''
+     
+     uoft_timetable_url = "http://www.artsandscience.utoronto.ca/ofr/timetable/winter/sponsors.htm"
+     site_content = requests.get(uoft_timetable_url)
+     timetable_and_courses = BeautifulSoup(site_content.content)
+    
+     for li in timetable_and_courses.findAll('li'):
+          department = li.find('a', href=True)
+          deparment_links = department.get('href')
+          
+          if len(deparment_links) > 10:
+               continue
+          
+          courses_and_schedules_url = "http://www.artsandscience.utoronto.ca/ofr/timetable/winter/" + deparment_links
+          print parse_course_offerings(courses_and_schedules_url)    
           
  
 def get_main_sched_time(course_info, courses, index):
      ''' Retrieves the first offered class schedule time for each courses and adds it in the courses collection '''
      
-     if (len(course_info) > 7) and not(re.match(r'\xc2\xa0',course_info[0].encode('utf-8'))):
-          course_code=cleanText(course_info[0].encode('utf-8'))
-          courses[course_code].append([cleanText(course_info[index].encode('utf-8')) for index in [2,3,5,6,7]])  
-          
+     course_code=clean_text(course_info[0].encode('utf-8'))
+     if (len(course_info) > 7 and course_code != 'Course'):
+          if not(regex_for_empt.match(course_info[0].encode('utf-8'))):
+               first_offered_shed = [clean_text(course_info[index].encode('utf-8')) for index in [2,3,5,6,7]]
+               courses[course_code].append(first_offered_shed)
+               
+     cancelled = len(course_info) >= 5 and clean_text(course_info[4]) == "Cancel"
+     
      return courses
 
 
-def get_other_sched_offered(course_info, other_sched, listCode, courses, index):
+def merge_course_info(course_info, other_sched, popular_courses, courses, index):
      ''' Retrieves the rest of schedule time offered per course, that is, if it offeres more than one schedule time. 
          Adds schedule in the courses collection '''
      
-     if re.match(r'\xc2\xa0',other_sched[0].encode('utf-8')):
-          if not(re.match(r'\xc2\xa0',course_info[0].encode('utf-8'))):
-               listCode.append(course_info[0])
-               
-     # Handles other schedule courses offered.        
-     if (len(course_info) > 7) and re.match(r'\xc2\xa0',course_info[0].encode('utf-8')):
-          course_code=cleanText(listCode[-1].encode('utf-8'))
-          courses[course_code].append([cleanText(course_info[index].encode('utf-8')) for index in [2,3,5,6,7]])
+     if not(regex_for_empt.match(course_info[0].encode('utf-8'))):
+          popular_courses.append(course_info[0])
+          
+     course_code=clean_text(popular_courses[-1].encode('utf-8'))         
+     if (len(course_info) > 7):
+          if regex_for_empt.match(course_info[0].encode('utf-8')):
+               other_schedule_offered = [clean_text(course_info[index].encode('utf-8')) for index in [2,3,5,6,7]]
+               courses[course_code].append([clean_text(course_info[index].encode('utf-8')) for index in [2,3,5,6,7]])
           
      return courses
      
-
-def parseTimetable():
-     ''' Returns a collection of courses offered by department. '''
-     Timetable = "http://www.artsandscience.utoronto.ca/ofr/timetable/winter/sponsors.htm"
-     requestTT = requests.get(Timetable)
-     soup = BeautifulSoup(requestTT.content)
-    
-     for li in soup.findAll('li'):
-          department = li.find('a', href=True)
-          getContents = department.contents[0]
-          fullLink = department.get('href')
-          
-          if len(fullLink) > 10:
-               continue
-          
-          fixedLink = "http://www.artsandscience.utoronto.ca/ofr/timetable/winter/" + fullLink
-          print parseCourseTable(fixedLink)     
+def clean_text(text):
+     ''' Returns a string of course description without the escape seq. characters (i.e \r,\n). '''
      
+     strip_string = regex_for_strip.sub('', text).rstrip(",")
+     final_string = regex_for_empt.sub("", strip_string)
+     return final_string
     
 
 if __name__ == "__main__":
 
-     parseTimetable()
+     parse_timetable()
