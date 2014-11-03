abb78043329ecf799077c7d58413501fe42228d5
jixuan90 <xuan.ji90@gmail.com>
2014-11-03 14:01:35 -0500

    alternative attempt to clicking element

--- AbArticulus/rosi_parse/scheduleLoader.js
+++ AbArticulus/rosi_parse/scheduleLoader.js
@@ -1,4 +1,3 @@
-
 var page = require('webpage').create();
 var system = require('system');
 
@@ -8,35 +7,57 @@ var password = system.args[2];
 
 //Error handling
 page.onResourceError = function(resourceError){
-	page.reason=resourceError.errorString;
-	page.reason_url = resourceError.url;
+    page.reason=resourceError.errorString;
+    page.reason_url = resourceError.url;
 };
 
 
 page.open('http://www.rosi.utoronto.ca', function(status){
-	if (status !== 'success') {
-		console.log('FAIL to load the address ' 
-			+ page.reason_url 
-			+ " " 
-			+ page.reason);
-	} else {
-		//Parse the stuff in ROSI
-		page.onResourceRequested = function(request) {
-  			console.log('Request ' + JSON.stringify(request, undefined, 4));
-		};
+    if (status !== 'success') {
+        console.log('FAIL to load the address ' + page.reason_url + " " + page.reason);
+    } else {
+        //Parse the stuff in ROSI
+        page.onResourceRequested = function(request) {
+              console.log('Request ' + JSON.stringify(request, undefined, 4));
+        };
 
-		page.onResourceReceived = function(response) {
-  			console.log('Receive ' + JSON.stringify(response, undefined, 4));
-		};
+        page.onResourceReceived = function(response) {
+              console.log('Receive ' + JSON.stringify(response, undefined, 4));
+        };
+       
+       // page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
+        page.evaluate(function() {
+              function click(el){
+                  var ev = document.createEvent("MouseEvent");
+                  ev.initMouseEvent(
+                      "click",
+                      true /* bubble */, true /* cancelable */,
+                      window, null,
+                      0, 0, 0, 0, /* coordinates */
+                      false, false, false, false, /* modifier keys */
+                      0 /*left*/, null
+                  );
+           
+                  el.dispatchEvent(ev);
+              }
+      
+              var el = document.querySelector('.rosi-login-content a');
+              click(el);   
+        });
 
-		page.evaluate(function() {
-			$(".secondary-btn").click(function(){
-			});
-		});
-		
-			page.render("rosi.png");	
+	console.log("about to run finish");
+        page.onLoadFinished = function(status){
+            if(status !== 'success'){
+              console.log('FAIL to load the address ' + page.reason_url + " " + page.reason);
+            }else{
+              console.log("yeyeyeyey");
+              page.render('rosi.png');
+            }
+        }
+        console.log("finishing now");
+        phantom.exit();
+        //});
+    }
 
-	}
-	phantom.exit();
-
+ 
 });
