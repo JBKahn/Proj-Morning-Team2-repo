2963f27648823e5f493e25be249d94c18f903f5a
LLay <lay.lyon@gmail.com>
2014-11-03 10:45:31 -0500

    controller in right place?

--- AbArticulus/time_table/static_src/js/controllers/ngCalendarCtrl.js
+++ AbArticulus/time_table/static_src/js/controllers/ngCalendarCtrl.js
@@ -33,3 +33,30 @@ var CalendarController =  function($scope, EventService) {
 };
 angular.module("timeTable.controllers.calendar", [])
 .controller("CalendarController", ["$scope", "EventService", CalendarController]);
+
+
+
+angular.module('ui.bootstrap.demo').controller('ModalDemoCtrl', function ($scope, $modal, $log) {
+
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'myModalContent.html',
+      controller: 'EventModalController',
+      size: size,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+});
