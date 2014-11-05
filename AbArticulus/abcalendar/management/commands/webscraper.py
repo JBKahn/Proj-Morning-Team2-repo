36b8ad7f63649ac301bf19145a737d370db7461d
Joseph Kahn <josephbkahn@gmail.com>
2014-11-05 03:10:12 -0500

    WIP

--- AbArticulus/abcalendar/management/commands/webscraper.py
+++ AbArticulus/abcalendar/management/commands/webscraper.py
@@ -1,57 +1,49 @@
 import pytz
+import re
 from datetime import datetime, timedelta
-from abcalendar.time_table_parser import University_of_Toronto_Timetable, get_lecture_info
-from django.core.management.base import BaseCommand
 from string import ascii_letters
-from abcalendar.models import Tag, Organization, Event
+
+from django.core.management.base import BaseCommand
+
+from abcalendar.time_table_parser import University_of_Toronto_Timetable, get_lecture_info
+from api.interfaces.api_interface import ApiInterface
 from authentication.models import CustomUser
-# from api.interfaces.api_interface import create_event_json, post_event_to_calendar, get_calendars_from_user
 
 
 class Command(BaseCommand):
     help = 'Adds a course in the timetable database.'
 
     def handle(self, *args, **options):
+        calendar_id = 'primary'
         if CustomUser.objects.filter(is_staff=True).exists():
             user = CustomUser.objects.filter(is_staff=True)[0]
         else:
             print "This command requires a staff account to exist. Please create one and try again"
-            return False
+            return
         timetable_info = self.timetable_information()
         for course, course_info in timetable_info.iteritems():
-            # creates each class as an organization object
-            # class_org = Organization.objects.get_or_create(name=course, user=customUser)
-
             # for each course, create a tag object
             for lecture in course_info:
 
                 # returns course_desc, course_section, course_time, course_loc, course_instructor
                 course_desc, course_sec, course_time, course_loc, instructor = get_lecture_info(lecture)
-                # course_title = course + " : " + course_desc
-
-                # creates a tag object for each classes created
-                # lecture_tags = Tag.objects.get_or_create(tag_type='LECTURE', class_section=course_sec, number=0)
-
+                course_title = course + " : " + course_desc
                 lecture_times = self.convert_lecture_time_codes(course_time)
 
                 # Hard coding Y till I figure out where you put it.
                 for weekday, start_time, duration in lecture_times:
                     event_start, event_end, recur_until = self.get_reccuring_time_until('Y', weekday, start_time, duration)
-                    # how to work on start, end day?
-                    # json_create_eve = create_event_json(title=course_title, start=start_time, end=end_time, all_day=False, description=course_desc, location=course_loc)
 
-                    # cal_info = post_event_to_calendar(user=customUser, calendar_id='primary', event=json_create_eve)
-
-                    # if cal_info['id'] != '':
-                    #  time_table_event = Event.objects.create(gevent_id=cal_info['id'],tag=lecture_tags,user=customUser)
-                    # else:
-                    #  raise ValueError("Failed to retrieve gevent_id.")
-        return True
+                    try:
+                        event = ApiInterface.create_google_json(title=course_title, start=event_start, end=event_end, all_day=False, description=None, location=None, recur_until=recur_until)
+                        ApiInterface.post_event_to_calendar(user=user, calendar_id=calendar_id, event=event, tag="LECTURE", org=course)
+                    except Exception:
+                        print "failed to add course: {}".format(course_title)
+        return
 
     def get_reccuring_time_until(self, time_period, day_of_week, hour_start, duration):
         if hour_start < 9:
             hour_start += 12
-        local = pytz.timezone("America/Toronto")
         if time_period == "F":
             # A monday
             start_date = datetime(year=2014, month=9, day=8, hour=0, minute=0, second=0)
@@ -75,17 +67,29 @@ class Command(BaseCommand):
         elif day_of_week == 'F':
             day_diff = 5
 
-        event_start = local.localize(start_date + timedelta(days=day_diff, hours=hour_start))
-        event_end = local.localize(event_start + timedelta(hours=duration))
+        local = pytz.timezone("America/Toronto")
+        event_start = local.localize(start_date + timedelta(days=day_diff, hours=int(hour_start)))
+        event_end = local.localize(start_date + timedelta(days=day_diff, hours=int(hour_start) + int(duration)))
         until = local.localize(end_date)
 
         return event_start, event_end, until
 
     def convert_lecture_time_codes(self, time_code_string):
+        # TODO: Michelle, empty courses can get in.
+        if not time_code_string:
+            return []
+        time_code_string = filter(lambda p: not p.strip().startswith('(Note') and p.strip(), time_code_string.split(','))[0]
+        time_code_string = re.sub(r'\([^])]*\)', '', time_code_string).strip().replace('am', '').replace('pm', '')
         backwards_timecode = time_code_string[::-1]
         current_time = ''
         lecture_times = []
         reset_time = False
+        # TODO: FIX
+        if time_code_string == 'Cancel' or len(time_code_string) == 1:
+            #TODO: Michelle, cancelled courses can get in.
+            return lecture_times
+        if time_code_string in ['TBA', 'See Details']:
+            return lecture_times
         for char in backwards_timecode:
             if char not in ascii_letters:
                 if reset_time:
@@ -94,17 +98,31 @@ class Command(BaseCommand):
                 current_time = char + current_time
             else:
                 split_times = current_time.split('-')
-                if int(split_times[0]) < 9:
-                    split_times[0] = int(split_times[0]) + 12
-                start = split_times[0]
 
+                if split_times[0].endswith(':15'):
+                    split_times[0] = int(split_times[0][:-3]) + .25
+                elif split_times[0].endswith(':30'):
+                    split_times[0] = int(split_times[0][:-3]) + .5
+                elif split_times[0].endswith(':45'):
+                    split_times[0] = int(split_times[0][:-3]) + .75
+
+                if float(split_times[0]) < 9:
+                    split_times[0] = float(split_times[0]) + 12
+                start = split_times[0]
                 if len(split_times) == 1:
                     duration = 1
                 else:
-                    if int(split_times[1]) < 9:
-                        split_times[1] = int(split_times[1]) + 12
-                    duration = int(split_times[1]) - int(split_times[0])
 
+                    if split_times[1].endswith(':15'):
+                        split_times[1] = int(split_times[1][:-3]) + .25
+                    elif split_times[1].endswith(':30'):
+                        split_times[1] = int(split_times[1][:-3]) + .5
+                    elif split_times[1].endswith(':45'):
+                        split_times[1] = int(split_times[1][:-3]) + .75
+
+                    if int(split_times[1]) < 9:
+                        split_times[1] = float(split_times[1]) + 12
+                    duration = float(split_times[1]) - float(split_times[0])
                 lecture_times.append((char, start, duration))
                 reset_time = True
         return lecture_times
