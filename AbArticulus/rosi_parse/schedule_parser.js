5038bf6c072240fc8d4c5a28ad55ea6c5d86c7d2
jixuan90 <xuan.ji90@gmail.com>
2014-11-06 01:37:06 -0500

    parsing the courses and printing them out

--- AbArticulus/rosi_parse/schedule_parser.js
+++ AbArticulus/rosi_parse/schedule_parser.js
@@ -11,7 +11,7 @@ casper.then(function(){
     this.sendKeys('#pin', password);
 });
 
-var courses = [];
+var course_name = [];
 
 //If login failed
 casper.thenClick('.button[value="Login"]', function(){
@@ -26,11 +26,33 @@ casper.thenClick(x('//a[text()="Personal Timetable"]'));
 //Fall term
 casper.thenClick('html body#subpage div#wrapper div#content.content div#right table tbody tr td.section table.decorated tbody tr td form#sessionForm input.button');
 
+casper.then(function(){
+    courses = this.getElementsInfo('tr[valign="top"] td:first-child');
+    //Get the course section
+    
+    this.echo(courses);
+    require('utils').dump(courses);
+});
 
+/*
+casper.evaluate(function(){
+     var rows = document.querySelector('td');
+     for(var i = 0; i < rows.length; i++ ){
+          courses.push(rows[i].text);
+     }    
+});
+
+
+
+casper.start.each(courses, function(self, course){
+    this.echo(course);
+});
 
 
 //Winter term
 casper.thenClick('html body#subpage div#wrapper div#content.content div#right table tbody tr td.section table.decorated tbody tr td form#sessionForm input.button');
+
+*/
 
 
 casper.then(function(){
