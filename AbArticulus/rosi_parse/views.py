9ad2397fb382ea684039a37211972868627eebd8
jixuan90 <xuan.ji90@gmail.com>
2014-11-06 00:39:19 -0500

    views execute casper js upon post request

--- AbArticulus/rosi_parse/views.py
+++ AbArticulus/rosi_parse/views.py
@@ -1,7 +1,8 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+import subprocess
+import sys
 from rosi_parse.forms import ROSIForm
 
 
@@ -10,6 +11,7 @@ class RosiParseAPIView(APIView):
     Accepts ROSI credentials and calls phantom JS and returns the course codes.
     Only accepts post requests.
     """
+       
 
     def post(self, request, format='JSON', *args, **kwargs):
         form = ROSIForm(request.POST)
@@ -18,5 +20,11 @@ class RosiParseAPIView(APIView):
             return Response(form.erors, status=status.HTTP_400_BAD_REQUEST)
 
         # TODO: invoke your script here tp get the course codes.
+        studentID = form.clean_student_num
+        password = form.clean_password
+        print >> sys.stderr,  "hihhihiih %s" %studentID 
+        #Run casperjs along with the parameters
+        raw = subprocess.check_output(['./casperjs schedule_parser.js ' + studentID + ' ' + password])
+        print "%s" %raw
         course_codes = []
         return Response({"course_codes": course_codes})
